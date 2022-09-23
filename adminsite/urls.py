b3aae7081ffe24fff48851fa1aeee1c019c003d4
mutemip <paulmutemi2030@gmail.com>
2022-09-23 15:05:23 +0300

    achanged to class based views

--- adminsite/urls.py
+++ adminsite/urls.py
@@ -5,10 +5,16 @@ from . import views
 
 app_name = 'adminsite'
 urlpatterns = [
-    # Add path here
-    path('', views.popular_course_list, name='popular_course_list'),
-    path('course/<int:course_id>/enroll/', views.enroll, name="enroll"),
-    path('course/<int:course_id>/', views.course_details, name='course_details'),
+    # Path for Function based Views
+
+    # path('', views.popular_course_list, name='popular_course_list'),
+    # path('course/<int:course_id>/enroll/', views.enroll, name="enroll"),
+    # path('course/<int:course_id>/', views.course_details, name='course_details'),
+
+    # pathe for class based views
+    path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
+    path(route='course/<int:pk>/enroll/', view=views.EnrollView.as_view(), name='enroll'),
+    path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- adminsite/views.py
+++ adminsite/views.py
@@ -1,41 +1,81 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Course, Lesson, Enrollment
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 from django.http import Http404
 
-# Create your views here.
-def popular_course_list(request):
-    context = {}
-    # If the request method is GET
-    if request.method == 'GET':
-        # Using the objects model manage to read all course list
-        # and sort them by total_enrollment descending
-        course_list = Course.objects.order_by('-total_enrollment')[:10]
-        # Appen the course list as an entry of context dict
-        context['course_list'] = course_list
+
+
+# #Function based views
+
+# #Function-based course list view
+# def popular_course_list(request):
+#     context = {}
+#     # If the request method is GET
+#     if request.method == 'GET':
+#         # Using the objects model manage to read all course list
+#         # and sort them by total_enrollment descending
+#         course_list = Course.objects.order_by('-total_enrollment')[:10]
+#         # Appen the course list as an entry of context dict
+#         context['course_list'] = course_list
+#         return render(request, 'adminsite/course_list.html', context)
+
+
+# def enroll(request, course_id):
+#     if request.method == 'POST':
+#         course = get_object_or_404(Course, pk=course_id)
+#         course.total_enrollment += 1
+#         course.save()
+#         return HttpResponseRedirect(reverse('adminsite:course_details', args=(course.id, )))
+
+
+# #Function-based course list view
+# def course_details(request, course_id):
+#     context = {}
+#     if request.method == 'GET':
+#         try:
+#             course = Course.objects.get(pk=course_id)
+#             context['course'] = course
+#             # Use render() method to generate HTML page by combining
+#             # template and context
+#             return render(request, 'adminsite/course_detail.html', context)
+#         except Course.DoesNotExist:
+#             # If course does not exist, throw a Http404 error
+#             raise Http404("No course matches the given id.")
+
+
+
+# class-based views
+class CourseListView(View):
+    def get(self, request):
+        context = {}
+        course_list = Course.objects.order_by('-total_enrolled')[:10]
+        context = {
+            'course_list': course_list
+        }
         return render(request, 'adminsite/course_list.html', context)
 
 
-def enroll(request, course_id):
-    if request.method == 'POST':
+class EnrollView(View):
+    def post(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
         course = get_object_or_404(Course, pk=course_id)
         course.total_enrollment += 1
         course.save()
         return HttpResponseRedirect(reverse('adminsite:course_details', args=(course.id, )))
 
-def course_details(request, course_id):
-    context = {}
-    if request.method == 'GET':
+class CourseDetailsView(View):
+    def get(self, request, *args, **kwags):
+        context = {}
+        course_id = kwags.get('pk')
         try:
             course = Course.objects.get(pk=course_id)
-            context['course'] = course
-            # Use render() method to generate HTML page by combining
-            # template and context
+            context = {
+                'course': course
+            }
             return render(request, 'adminsite/course_detail.html', context)
         except Course.DoesNotExist:
-            # If course does not exist, throw a Http404 error
-            raise Http404("No course matches the given id.")
-
+            raise Http404('The course with given Id is not available!!')
